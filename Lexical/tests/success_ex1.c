3a01a24132fba84d7c80b0ab5054b377b041fe69
Heitor <heitorbelem21@gmail.com>
2021-08-07 23:39:55 -0300

    ✅ Improved example files

--- Lexical/tests/success_ex1.c
+++ Lexical/tests/success_ex1.c
@@ -1,21 +1,30 @@
-int main(int i, float j) {
+int main() {
     int k;
-    read(k);
-    write(k);
-    int _result1;
-    int res_2_b;
-    if(i < j){
-        j = 1.2;
-        i = 2;
-        _result1 = j - i;
-        write(k);
-    } else {
-        i = 1;
-        j = .5;
-        res_2_b = i + j;
-        write(k);
+    k = 0;
+    
+    int _max;
+    _max = 0;
+
+    int sum_1;
+    float mul_2;
+
+    sum_1=0;
+    mul_2=1.0;
+
+    for(k = 0; k < _max; k++) {
+        sum_1 += k;
+        mul_2 *= k*.2;
     }
 
-    write(res_2_b - _result1);
+    if( !(sum_1 % 2) > 100) {
+        write(sum_1);
+    } else if(sum_1*2 < 20) {
+        write(sum_1--);
+    }
+
+    if(mul_2 == 10) {
+        write(mul_2/2);
+    }
+
     return 0;
 }
